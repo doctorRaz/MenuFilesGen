5ae31db3b9085dc1ad9288473b236a0c634f7d42
doctorRaz <36532296+doctorRaz@users.noreply.github.com>
2025-07-25 10:19:23 +0300

    примеры

---
+++ example/menuGenTSV.bat
@@ -0,0 +1,2 @@
+"d:\@Developers\Programmers\!NET\GitHubMy\MenuFilesGen\src\MenuFilesGen\bin\Release\MenuFilesGen.exe" -exo:0 -hrr:3 -xpn:1 "d:\@Developers\Programmers\!NET\!bundle\BlockFix.bundle\Resources" "d:\@Developers\Programmers\!NET\GitHubMy\MenuFilesGen\example\BlockFix.tsv"
+
