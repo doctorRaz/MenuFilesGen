828a532bbcc018fb5731a70230916a18ea2ba3dc
doctorRAZ <doctorRAZ@yandex.ru>
2025-08-04 22:48:44 +0300

    заготовка для меню dRzTools

---
+++ example/drzTools.bat
@@ -0,0 +1,2 @@
+"d:\@Developers\Programmers\!NET\!MenuFilesGen\MenuFilesGen\src\MenuFilesGen\bin\Release\MenuFilesGen.exe" -exo:1 -hrr:3 -xpn:1 "d:\@Developers\Programmers\!NET\!bundle\BlockFix.bundle\Resources" "d:\@Developers\Programmers\!NET\!MenuFilesGen\MenuFilesGen\example\drzTools.xlsm"
+
